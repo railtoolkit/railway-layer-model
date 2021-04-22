fc2381ed8c1deee9f24fd0c728b0c94ac5b95ce3
Martin Scheidt <kaat0@ownx.net>
2021-04-22 15:04:12 +0200

    moved common graph functions to LMcore.jl

--- src/BaseLayer.jl
+++ src/BaseLayer.jl
@@ -7,139 +7,36 @@
 
 module BaseLayer
 
-using YAML
-using LightGraphs, MetaGraphs
-using GraphPlot
+include("LMcore.jl")
 
-export loadBaseLayer, saveBaseLayer, showBaseLayer
+export loadBaseLayer, saveBaseLayer
 
 # ===========================
 """
-Takes a YAML-file and extracts the "network" from it.
+Takes a YAML-file and extracts the Base Layer Attributes.
 Returns a MetaGraph object.
 """
 function loadBaseLayer(file_path)
 
-  # load data from external file
-  # TODO error handling
-  data = YAML.load(open(file_path))
+  graph_name = "base_layer"
+  node_name  = "stations"
+  edge_name  = "lines"
 
-  # determine number of nodes/stations in external file
-  numStations = length(data["base_layer"]["stations"])
-  
-  # map station ids to a number
-  stationID2Num = Dict()
-  for station in 1:numStations
-    push!(stationID2Num, data["base_layer"]["stations"][station]["id"] => station)
-  end
-
-  # construct MetaDiGraph with station and connections
-  network = MetaGraphs.MetaDiGraph(numStations)
-  # add generell graph informations
-  MetaGraphs.set_prop!(network, :name, data["base_layer"]["name"])
-
-  # add properties for the stations (vertices)
-  for station in 1:numStations
-    station_props = data["base_layer"]["stations"][station]
-    # make sure the station has an id and remove it from the Dict
-    MetaGraphs.set_prop!( network, station, :id, pop!(station_props,"id"))
-    # add remaining properties
-    for station_prop in station_props
-      MetaGraphs.set_prop!(network, station, Symbol(station_prop[1]), station_prop[2])
-    end
-  end
-
-  # add properties for the links (edges)
-  for link in data["base_layer"]["lines"]
-    # make sure the link has a source and a target and remove them from Dict
-    source = stationID2Num[pop!(link,"source")]
-    target = stationID2Num[pop!(link,"target")]
-    # adding the link to the graph
-    LightGraphs.add_edge!(network, source, target)
-    # add remaining properties
-    for link_prop in link
-      MetaGraphs.set_prop!(network, source, target, Symbol(link_prop[1]), link_prop[2])
-    end
-  end
-
-  return network # MetaDiGraph object
+  return LMcore.loadGraph(file_path, graph_name, node_name, edge_name)
 
 end # function loadBaseLayer
 
 # ===========================
 """
-Takes a MetaGraph object with a base layer and prints it.
+Takes a Base Layer MetaGraph object and converts it to a YAML-file.
 """
-function showBaseLayer(network::AbstractMetaGraph)
+function saveBaseLayer(baseLayer, file_path)
 
-  nodeNames = []
-  locs_x = Float64[]
-  locs_y = Float64[]
-  for item in 1:LightGraphs.nv(network.graph)
-    push!(nodeNames, MetaGraphs.get_prop(network, item, :name))
-    coord_x, coord_y = MetaGraphs.get_prop(network, item, :plot_coord)
-    push!(locs_x, coord_x)
-    push!(locs_y, coord_y)
-  end
+  graph_name = "base_layer"
+  node_name  = "stations"
+  edge_name  = "lines"
 
-  edgeNames = []
-  for item in LightGraphs.edges(network)
-    source, target = LightGraphs.src(item), LightGraphs.dst(item)
-    push!(edgeNames, MetaGraphs.get_prop(network, source, target, :name))
-  end
-
-  GraphPlot.gplot(
-    network.graph,
-    locs_x, locs_y,
-    nodelabel=nodeNames,
-    NODESIZE=0.05,
-    nodelabeldist=1,
-    nodelabelangleoffset=4/π,
-    edgelabel=edgeNames
-  )
-
-end # function showBaseLayer
-
-# ===========================
-"""
-Takes a MetaGraph object and converts it to a YAML-file.
-"""
-function saveBaseLayer(network::AbstractMetaGraph, file_path)
-
-  data = Dict()
-  push!(data, :base_layer => Dict())
-  push!(data[:base_layer], :name => MetaGraphs.get_prop(network, :name))
-  stations = []
-  for item in 1:LightGraphs.nv(network.graph)
-    station = Dict()
-    for (key,value) in MetaGraphs.props(network, item)
-      push!(station, key => value)
-    end
-    push!(stations, station)
-  end
-  push!(data[:base_layer], :stations => stations)
-
-  # map station number to an id 
-  stationNum2ID = []
-  for item in 1:LightGraphs.nv(network.graph)
-    push!(stationNum2ID, MetaGraphs.get_prop(network, item, :id))
-  end
-
-  lines = []
-  for item in LightGraphs.edges(network)
-    line = Dict()
-    source, target = LightGraphs.src(item), LightGraphs.dst(item)
-    push!(line, :source => stationNum2ID[source])
-    push!(line, :target => stationNum2ID[target])
-    for (key,value) in MetaGraphs.props(network, source, target)
-      push!(line, key => value)
-    end
-    push!(lines, line)
-  end
-  push!(data[:base_layer], :lines => lines)
-
-  # TODO error handling
-  YAML.write_file(file_path, data)
+  LMcore.saveGraph(baseLayer, file_path, graph_name, node_name, edge_name)
 
 end # function saveBaseLayer
 
---
+++ src/LMcore.jl
@@ -0,0 +1,201 @@
+#!/usr/bin/env julia
+# -*- coding: UTF-8 -*-
+# __julia-version__ = 1.6.0
+# __author__        = "Martin Scheidt"
+# __copyright__     = "2018-2021"
+# __license__       = "ISC"
+
+module LMcore
+
+using YAML
+using LightGraphs, MetaGraphs
+using GraphPlot
+
+export loadGraph, saveBaseLayer, showBaseLayer
+
+# ===========================
+"""
+Takes a YAML-file and extracts the "network" from it.
+Returns a MetaGraph object.
+"""
+function loadGraph(file_path, graph_name, node_name, edge_name)
+
+  # load data from external file
+  # TODO error handling
+  data = YAML.load(open(file_path))
+
+  # determine number of nodes/stations in external file
+  numNodes = length(data[graph_name][node_name])
+  
+  # map node ids to a number
+  nodeID2Num = Dict()
+  for node in 1:numNodes
+    push!(nodeID2Num, data[graph_name][node_name][node]["id"] => node)
+  end
+
+  # construct MetaDiGraph with node and edges
+  graph = MetaGraphs.MetaDiGraph(numNodes)
+  # add generell graph informations
+  MetaGraphs.set_prop!(graph, :name, data[graph_name]["name"])
+
+  # add properties for the nodes (vertices)
+  for node in 1:numNodes
+    node_props = data[graph_name][node_name][node]
+    # make sure the station has an id and remove it from the Dict
+    MetaGraphs.set_prop!(graph, node, :id, pop!(node_props,"id"))
+    # add remaining properties
+    for prop in node_props
+      MetaGraphs.set_prop!(graph, node, Symbol(prop[1]), prop[2])
+    end
+  end
+
+  # add properties for the edges
+  for edge in data[graph_name][edge_name]
+    # make sure the edge has a source and a target and remove them from Dict
+    source = nodeID2Num[pop!(edge,"source")]
+    target = nodeID2Num[pop!(edge,"target")]
+    # adding the edge to the graph
+    LightGraphs.add_edge!(graph, source, target)
+    # add remaining properties
+    for prop in edge
+      MetaGraphs.set_prop!(graph, source, target, Symbol(prop[1]), prop[2])
+    end
+  end
+
+  return graph # MetaDiGraph object
+
+end # function loadGraph
+
+# ===========================
+"""
+Takes a MetaGraph object and converts it to a YAML-file.
+"""
+function saveGraph(graph::AbstractMetaGraph, file_path, graph_name, node_name, edge_name)
+  graph_name = Symbol(graph_name)
+  node_name  = Symbol(node_name)
+  edge_name  = Symbol(edge_name)
+
+  data = Dict()
+  push!(data, graph_name => Dict())
+  push!(data[graph_name], :name => MetaGraphs.get_prop(graph, :name))
+  stations = []
+  for item in 1:LightGraphs.nv(graph.graph)
+    station = Dict()
+    for (key,value) in MetaGraphs.props(graph, item)
+      push!(station, key => value)
+    end
+    push!(stations, station)
+  end
+  push!(data[graph_name], node_name => stations)
+
+  # map station number to an id 
+  stationNum2ID = []
+  for item in 1:LightGraphs.nv(graph.graph)
+    push!(stationNum2ID, MetaGraphs.get_prop(graph, item, :id))
+  end
+
+  lines = []
+  for item in LightGraphs.edges(graph)
+    line = Dict()
+    source, target = LightGraphs.src(item), LightGraphs.dst(item)
+    push!(line, :source => stationNum2ID[source])
+    push!(line, :target => stationNum2ID[target])
+    for (key,value) in MetaGraphs.props(graph, source, target)
+      push!(line, key => value)
+    end
+    push!(lines, line)
+  end
+  push!(data[graph_name], edge_name => lines)
+
+  # TODO error handling
+  YAML.write_file(file_path, data)
+
+end # function saveBaseLayer
+
+# ===========================
+"""
+Takes a MetaGraph object with a base layer and prints it.
+"""
+function showGraph(graph::AbstractMetaGraph)
+
+  # test if graph has special properties
+  graph_has_prop_node_names = hasNodeProp(graph, :name)
+  graph_has_prop_edge_names = hasEdgeProp(graph, :name)
+  graph_has_prop_plot_coord = hasNodeProp(graph, :plot_coord)
+
+  nodeNames = []
+  for item in 1:LightGraphs.nv(graph.graph)
+    if graph_has_prop_node_names
+      push!(nodeNames, MetaGraphs.get_prop(graph, item, :name))
+    else
+      push!(nodeNames, MetaGraphs.get_prop(graph, item, :id))
+    end
+  end
+
+  edgeNames = []
+  for item in LightGraphs.edges(graph)
+    source, target = LightGraphs.src(item), LightGraphs.dst(item)
+    if graph_has_prop_edge_names
+      push!(edgeNames, MetaGraphs.get_prop(graph, source, target, :name))
+    else
+      push!(edgeNames, MetaGraphs.get_prop(graph, source, target, :id))
+    end
+  end
+
+  if graph_has_prop_plot_coord
+    locs_x = Float64[]
+    locs_y = Float64[]
+    for item in 1:LightGraphs.nv(graph.graph)
+      coord_x, coord_y = MetaGraphs.get_prop(graph, item, :plot_coord)
+      push!(locs_x, coord_x)
+      push!(locs_y, coord_y)
+    end
+
+    GraphPlot.gplot(
+      graph.graph,
+      locs_x, locs_y,
+      nodelabel=nodeNames,
+      NODESIZE=0.05,
+      nodelabeldist=1,
+      nodelabelangleoffset=4/π,
+      edgelabel=edgeNames
+    )
+    
+  else
+
+    GraphPlot.gplot(
+      graph.graph,
+      nodelabel=nodeNames,
+      NODESIZE=0.05,
+      nodelabeldist=1,
+      nodelabelangleoffset=4/π,
+      edgelabel=edgeNames
+    )
+
+  end
+
+end # function showGraph
+
+function hasNodeProp(graph::AbstractGraph, prop::Symbol)
+  try
+    MetaGraphs.get_prop(graph, 1, prop)
+    catch error
+    if isa(error, KeyError)
+      return false
+    end
+  end
+  return true
+end # function hasNodeProp
+
+function hasEdgeProp(graph::AbstractGraph, prop::Symbol)
+  try
+    MetaGraphs.get_prop(graph, collect(edges(graph))[1], prop)
+    catch error
+    if isa(error, KeyError)
+      return false
+    end
+  end
+  return true
+end # function hasEdgeProp
+
+end # module LMcore
